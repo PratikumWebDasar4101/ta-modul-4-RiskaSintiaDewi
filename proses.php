2feaa6ed484b07e950620d3beab018e5be37df40
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-25 05:43:28 -0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,24 @@
-<?php
+<?php  
+
+foreach($_REQUEST as $name => $value){
+  if (is_array($value)) {
+    echo "$name : <br>";
+
+    $items = $value; 
+    foreach ($items as $item) {
+      echo "  $item <br>";
+    }
+  } else {
+    echo "$name: $value <br>";
+  }
+}
+
+$file = $_FILES['dokumen'];
+$nama_file = $file['name'];
+$nama_tmp = $file['tmp_name'];
+$upload_dir = "upload/";
+move_uploaded_file ($nama_tmp,$upload_dir.$nama_file);
 
 ?>
+<img width='500px' src="upload/<?php echo $nama_file; ?>">
+<br><a href="proses_login.php"><input type="submit" name="Logout" value="Logout">
