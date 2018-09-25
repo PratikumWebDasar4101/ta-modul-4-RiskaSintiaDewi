2f826f9ef37b025b305457a811a79b0c16545218
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-25 05:44:20 -0700

    Update proses_login.php

--- proses_login.php
+++ proses_login.php
@@ -1,3 +1,12 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form method="post" action="formawal.html" enctype="multipart/form-data">
+		Username : <input type="text" name="username"><br><br>
+		Password : <input type="password" name="password"><br><br>
+		<input type="submit" name="submit" value="Submit">
+</body>
+</html>
