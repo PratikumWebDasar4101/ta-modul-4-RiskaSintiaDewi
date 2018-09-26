37efd6cbbf0e56519db95f667c80b0533424dedd
RiskaSintiaDewi <42725266+RiskaSintiaDewi@users.noreply.github.com>
2018-09-26 08:12:05 -0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,5 +1,20 @@
 <?php  
 
+session_start();
+
+$user = array(
+	"username" => "admin",
+	"password" => 123
+);
+
+if ($user["username"] == $_POST["username"] && $user["password"] == $_POST["password"]) {
+	$_SESSION["username"] = $user["username"];
+	$_SESSION["user"] = $user;
+	header("Location:halaman_belanja.php");
+}else {
+	header("Location:login.php");
+}
+
 foreach($_REQUEST as $name => $value){
   if (is_array($value)) {
     echo "$name : <br>";
